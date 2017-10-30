8f63273b7badcfc11ca4e50be6a539ca4dfdbe08
cmg <desarrollo@cmg.com.co>
2017-10-30 16:25:12 -0500

    wip

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,9 @@
-<<<<<<< HEAD
-
-
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
-
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
-=======
-set :application, 'comunidad'
+set :application, 'comunidad2'
 set :repo_url, 'https://github.com/RobertoRuedaQ/comunidad.git'
-set :branch, "master"
+set :branch, "comunidad2"
 set :deploy_via, :copy
-set :deploy_to, '/home/deploy/comunidad'
+set :deploy_to, '/home/deploy/comunidad2'
 set :user, 'deploy'
 
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
->>>>>>> 8a865410d16f6f33e581535dd39674f22a57fac7
