8102858d90ac0b515009668e511b76c5ae407f67
cmg <desarrollo@cmg.com.co>
2017-10-31 16:45:28 -0500

    comment the carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,27 +1,27 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
+# require 'carrierwave/storage/abstract'
+# require 'carrierwave/storage/file'
+# require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-  config.root = Rails.root.join('tmp') # adding these...
-  config.cache_dir = 'carrierwave' # ...two lines
-  if Rails.env.production?
-    config.storage :fog
-    config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'key',                        # required
-    :aws_secret_access_key  => 'secret',                     # required
-    :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
-    :host                   => 's3.example.com',             # optional, defaults to nil
-    :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
-  }
-  else
-    config.storage :file
-    config.enable_processing = false if Rails.env.test?
-  end
+# CarrierWave.configure do |config|
+#   config.root = Rails.root.join('tmp') # adding these...
+#   config.cache_dir = 'carrierwave' # ...two lines
+#   if Rails.env.production?
+#     config.storage :fog
+#     config.fog_provider = 'fog/aws'
+#   config.fog_credentials = {
+#     :provider               => 'AWS',                        # required
+#     :aws_access_key_id      => 'key',                        # required
+#     :aws_secret_access_key  => 'secret',                     # required
+#     :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
+#     :host                   => 's3.example.com',             # optional, defaults to nil
+#     :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+#   }
+#   else
+#     config.storage :file
+#     config.enable_processing = false if Rails.env.test?
+#   end
 
-  config.fog_directory  = 'directory'                             # required
-  config.fog_public     = false                                   # optional, defaults to true
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-end
+#   config.fog_directory  = 'directory'                             # required
+#   config.fog_public     = false                                   # optional, defaults to true
+#   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+# end
