9e9b215c89c70f64debab704b270becad4de7cfb
cmg <desarrollo@cmg.com.co>
2017-11-29 15:57:36 -0500

    wip

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
 
 Hobby.create(name: "Deporte")
@@ -28,7 +21,7 @@ csv.each do |row|
 end
 
 
-OrderStatus.create! id: 1, name: "In ProgreEn progreso"
+OrderStatus.create! id: 1, name: "En progreso"
 OrderStatus.create! id: 2, name: "Confirmado"
 OrderStatus.create! id: 3, name: "Entregado"
 OrderStatus.create! id: 4, name: "Cancelado"
